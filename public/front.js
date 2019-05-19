7b78d6040e016770718ce450bfe4ee38d8266526
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-05-18 18:47:44 -0700

    sidebar reappears when switching to landscape on tablet

--- public/front.js
+++ public/front.js
@@ -1,5 +1,9 @@
 let bSidebar = false;
 
+sizeChecker = setInterval(()=>{
+  if ((window.innerWidth > 992) && !bSidebar) showNav()
+}, 250)
+
 // shows and hides sidebar when in mobile mode
 $(document).on("click", ".icon", function (event) {
   if (!bSidebar) {
@@ -11,20 +15,22 @@ $(document).on("click", ".icon", function (event) {
 
 // hides sidebar if nav link is clicked
 $(document).on("click", ".n-link", () => {
-  hideNav();
+  if (window.innerWidth <= 992) {
+    hideNav();
+  }
 })
 
 const showNav = () => {
   console.log("show nav")
   $(".sidebar").css("transform", "translateX(0px)")
-  $(".icon").css("transform", "translateX(300px) scale(1.8,1.8)")
+  // $(".icon").css("transform", "translateX(300px) scale(1.8,1.8)")
   bSidebar = true;
 }
 
 const hideNav = () => {
   console.log("hide nav")
   $(".sidebar").css("transform", "translateX(-300px)")
-  $(".icon").css("transform", "translateX(0px) scale(1.8,1.8)")
+  // $(".icon").css("transform", "translateX(0px) scale(1.8,1.8)")
   bSidebar = false;
 
 }

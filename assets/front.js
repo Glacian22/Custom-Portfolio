ae2a4b1a58fb679e28c859204ce855a5229107e6
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-06-06 17:08:31 -0700

    fixed unclickable button

--- assets/front.js
+++ assets/front.js
@@ -4,6 +4,8 @@ let loc = document.location.href.split('#')[1];
 $(document).ready(function () {
   $(".cont").css("visibility", "hidden")
   $("." + loc).css("visibility", "visible")
+  $("." + loc).css("opacity", "1.0")
+  $("." + loc).css("transform", "scale(1.0)")
   $("#" + loc).attr("class", "n-link active")
 });
 
@@ -13,10 +15,7 @@ sizeChecker = setInterval(() => {
 
 locationChecker = setInterval(() => {
   const newLocation = document.location.href.split('#')[1];
-  console.log(`location: ${loc} newLocation: ${newLocation}`)
   if (loc !== newLocation) {
-    console.log(`new location: ${newLocation}`);
-
     // switches revealed section
     $("." + loc).css("transform", "scale(0.1)")
     $("." + loc).css("opacity", "0")
@@ -24,9 +23,8 @@ locationChecker = setInterval(() => {
 
     let oldLoc = loc;
     setTimeout(() => {
-      console.log(`oldLoc = ${oldLoc}`)
-      $("." + oldLoc).css("transform", "scale(8.0)")
-    }, 500)
+      $("." + oldLoc).css("transform", "scale(6.0)")
+    }, 800)
 
     $("." + newLocation).css("visibility", "visible")
     $("." + newLocation).css("opacity", "1.0")
@@ -49,6 +47,28 @@ $(document).on("click", ".icon", function (event) {
   }
 })
 
+const nextNav = () => {
+ // get the ID of whatever page we're on
+ console.log("hi")
+ let id = $("." + loc).attr("id");
+ id = 1 + id;
+ id > 3 ? id = 0 : null;
+ let nextPage = $("#" + id).attr("class").split(" ")[0];
+ console.log(`nextPage: ${nextPage}`)
+ // advance page to next ID
+ let url = document.location.href.split('#')[0];
+ window.location = url + "#" + nextPage;
+}
+
+$("#next").click(function(){
+  console.log("boo")
+})
+
+$(document).on("click", "#next", function(event){
+  console.log("wut")
+  nextNav();
+})
+
 const navClick = (link) => {
   // hides sidebar if nav link is clicked
   if (window.innerWidth <= 992)
@@ -56,20 +76,20 @@ const navClick = (link) => {
 }
 
 const showNav = () => {
-  console.log("show nav")
+  // console.log("show nav")
   $(".sidebar").css("transform", "translateX(0px)")
   bSidebar = true;
 }
 
 const hideNav = () => {
-  console.log("hide nav")
+  // console.log("hide nav")
   $(".sidebar").css("transform", "translateX(-300px)")
   bSidebar = false;
 }
 
 // onclick for the navlinks
 $(".n-link").click(() => {
-  console.log(this.id())
+  // console.log(this.id())
   if (this.id() === "active")
     null
 })
